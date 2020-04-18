3263682573bd84146e66ee24c4ee773dffd70caf
BhavyaShukla <bhavyashukla2002@gmail.com>
2020-04-18 18:15:36 +0200

    Updated String.cxx
    Implementations for WString.h

--- String.cxx
+++ String.cxx
@@ -15,179 +15,116 @@
  *  limitations under the License.
  */
 
-#include <algorithm>
-#include <cctype>
-#include <charconv>
-#include <cstring>
-#include <iostream>
-#include <regex>
-#include <string>
+#include <WString.h>
 
-enum StringBaseConv {
-    BIN = 2,
-    DEC = 10,
-    HEX = 16,
-};
+[[nodiscard]] inline auto c_str() const noexcept { return m_u.c_str(); }
+[[nodiscard]] inline auto length() const noexcept { return m_u.length(); }
+[[nodiscard]] inline auto charAt(unsigned idx) const noexcept { return m_u.at(idx); }
+[[nodiscard]] inline auto& charAt(unsigned idx) noexcept { return m_u.at(idx); }
+[[nodiscard]] inline auto operator[](unsigned idx) const noexcept { return m_u[idx]; }
+[[nodiscard]] inline auto& operator[](unsigned idx) noexcept { return m_u[idx]; }
 
-using byte = std::uint8_t;
+[[nodiscard]] inline int compareTo(const String& s) const noexcept {
+    return std::memcmp(m_u.c_str(), s.m_u.c_str(), (std::min)(s.m_u.size(), m_u.size()));
+}
 
-class String {
-    std::string m_u;
+[[nodiscard]] inline bool startsWith(const String& s) const noexcept {
+    if (s.m_u.size() > m_u.size())
+        return false;
+    return std::memcmp(m_u.c_str(), s.m_u.c_str(), s.m_u.size()) == 0;
+}
 
-    String(std::string u) : m_u{std::move(u)} {}
+[[nodiscard]] inline bool endsWith(const String& s) const noexcept {
+    if (s.m_u.size() > m_u.size())
+        return false;
+    return (m_u.compare(m_u.length() - s.m_u.length(), s.m_u.length(), s.m_u) == 0);
+}
 
-  public:
-    String() noexcept = default;
-    String(const String&) = default;
-    String(String&&) noexcept = default;
-    String& operator=(const String&) = default;
-    String& operator=(String&&) = default;
-
-    template <std::size_t N> inline String(const char (&charr)[N]) : m_u{charr, N} {}
-    inline String(const char* cstr) : m_u{cstr} {}
-    inline explicit String(char c) : m_u{1, c} {}
-    template <class T, class = std::enable_if_t<std::is_integral<T>::value>> inline String(T val, StringBaseConv base) {
-        constexpr static auto base2len_upperbound = [](StringBaseConv base) /*constexpr*/ {
-            switch (base) {
-            case BIN:
-                return 65;
-            case DEC:
-                return 11;
-            case HEX:
-                return 17;
-            default:
-                throw;
-            }
-        };
-        m_u.resize(base2len_upperbound(base));
-        const auto res = std::to_chars(&*m_u.begin(), &*m_u.rbegin(), val, +base);
-        if (static_cast<int>(res.ec))
-            throw;
-        m_u.resize(std::strlen(m_u.c_str()));
+inline void getBytes(byte buffer[], unsigned length) {
+    for (int i = 0; i < m_u.length; i++) {
+        buffer[i] = (byte)m_u[i];
     }
+}
 
-     // template <class T, class = std::enable_if_t<std::is_floating_point<T>::value>>
-    // String(T val, int precision); // unimplemented
+[[nodiscard]] inline int indexOf(const char* c) const noexcept { return m_u.find(c); }
 
-    [[nodiscard]] inline auto c_str() const noexcept { return m_u.c_str(); }
-    [[nodiscard]] inline auto length() const noexcept { return m_u.length(); }
-    [[nodiscard]] inline auto charAt(unsigned idx) const noexcept { return m_u.at(idx); }
-    [[nodiscard]] inline auto& charAt(unsigned idx) noexcept { return m_u.at(idx); }
-    [[nodiscard]] inline auto operator[](unsigned idx) const noexcept { return m_u[idx]; }
-    [[nodiscard]] inline auto& operator[](unsigned idx) noexcept { return m_u[idx]; }
+[[nodiscard]] inline int indexOf(const char* c, unsigned index) const noexcept { return m_u.find(c, index); }
 
-    template <class T> inline bool concat(const T& v) {
-        m_u += String(v).m_u;
-        return true;
+[[nodiscard]] inline int indexOf(const String& str) const noexcept { return m_u.find(str); }
+
+[[nodiscard]] inline int indexOf(const String& str, unsigned index) const noexcept { return m_u.find(str, index); }
+
+inline void remove(unsigned idx) { m_u.erase(index); }
+
+inline void remove(unsigned idx, unsigned count) { m_u.erase(idx, idx + count - 1); }
+
+inline void replace(const String& substring1, const String& substring2) {
+    size_t position = m_u.find(substring1.m_u);
+
+    while (position != std::string::npos) {
+        m_u.replace(position, m_u.size(), substring2.m_u);
+        position = m_u.find(substring1.m_u, position + substring2.m_u.size());
     }
+}
 
-    [[nodiscard]] inline int compareTo(const String& s) const noexcept {
-        return std::memcmp(m_u.c_str(), s.m_u.c_str(), (std::min)(s.m_u.size(), m_u.size()));
-    }
+inline void reserve(unsigned size) { m_u.reserve(size); }
 
-    [[nodiscard]] inline bool startsWith(const String& s) const noexcept {
-        if (s.m_u.size() > m_u.size())
-            return false;
-        return std::memcmp(m_u.c_str(), s.m_u.c_str(), s.m_u.size()) == 0;
-    }
+inline void setCharAt(unsigned index, const char* c) { m_u[index] = c; }
 
-    [[nodiscard]] inline bool endsWith(const String& s) const noexcept {
-        if (s.m_u.size() > m_u.size())
-            return false;
-        return (m_u.compare (m_u.length() - s.m_u.length(), s.m_u.length(), s.m_u) == 0);    
-    }
+[[nodiscard]] inline String substring(unsigned from) const { return m_u.substr(from); }
 
-    inline void getBytes(byte buffer [], unsigned length) { 
-        for (int i = 0; i < m_u.length; i++) {
-            buffer[i] = (byte)m_u[i];
+[[nodiscard]] inline String substring(unsigned from, unsigned to) const { return m_u.substr(from, to - from); }
+
+inline void toCharArray(const char* buffer[], unsigned length) noexcept {
+    if (length <= m_u.length) {
+        for (int i = 0; i < length; i++) {
+            buffer[i] = m_u[i];
+        }
+    } else {
+        for (i = 0; i < m_u.length; i++) {
+            buffer[i] = m_u[i];
         }
     }
+}
 
-    [[nodiscard]] inline int indexOf(const char* c) const noexcept { return m_u.find(c); }
-
-    [[nodiscard]] inline int indexOf(const char* c, unsigned index) const noexcept { return m_u.find(c, index); }
-
-    [[nodiscard]] inline int indexOf(const String& str) const noexcept { return m_u.find(str); }
-
-    [[nodiscard]] inline int indexOf(const String& str, unsigned index) const noexcept { return m_u.find(str, index); }
-
-    inline void remove(unsigned idx) { m_u.erase(index); }
-
-    inline void remove(unsigned idx, unsigned count) { m_u.erase(idx, idx + count - 1); }
-
-    inline void replace(const String& substring1, const String& substring2) { 
-       size_t position = m_u.find(substring1.m_u);
-
-       while (position != std::string::npos) {
-           m_u.replace(position, m_u.size(), substring2.m_u);
-           position = m_u.find(substring1.m_u, position + substring2.m_u.size());
-       }
-    }
-
-    inline void reserve(unsigned size) { m_u.reserve(size); }
-
-    inline void setCharAt(unsigned index, const char* c) { m_u[index] = c; }
-
-    [[nodiscard]] inline String substring(unsigned from) const { return m_u.substr(from); }
-
-    [[nodiscard]] inline String substring(unsigned from, unsigned to) const { return m_u.substr(from, to - from); }
-
-    inline void toCharArray(const char* buffer[], unsigned length) noexcept{ 
-        if (length <= m_u.length) {
-            for (int i = 0; i < length; i++) {
-                buffer[i] = m_u[i];
-            }
-        } else {
-            for ( i = 0; i < m_u.length; i++) {
-                buffer[i] = m_u[i];
-            }
-        }
-    }
-
-    [[nodiscard]] inline long toInt() const noexcept {
+[[nodiscard]] inline long toInt() const noexcept {
         if (isdigit(m_u[0]) {
-            return std::stoi(m_u);
+        return std::stoi(m_u);
         } else return 0;
-    }
+}
 
-    [[nodiscard]] inline double toDouble() const noexcept {
+[[nodiscard]] inline double toDouble() const noexcept {
         if (isdigit(m_u[0]) {
-            return std::stod(m_u);
+        return std::stod(m_u);
         } else return 0;
-    }
+}
 
-    [[nodiscard]] inline float toFloat() const noexcept {
+[[nodiscard]] inline float toFloat() const noexcept {
         if (isdigit(m_u[0]) {
-            return std::stof(m_u);
+        return std::stof(m_u);
         } else return 0;
-    }
+}
 
-    inline void toLowerCase() {std::transform (m_u.begin(), m_u.end(), m_u.begin(), std::tolower); }
+inline void toLowerCase() { std::transform(m_u.begin(), m_u.end(), m_u.begin(), std::tolower); }
 
-    inline void toUpperCase() {std::transform (m_u.begin(), m_u.end(), m_u.begin(), std::toupper); }
+inline void toUpperCase() { std::transform(m_u.begin(), m_u.end(), m_u.begin(), std::toupper); }
 
-    inline void trim() { m_u = m_u.erase(std::remove (m_u.begin(), m_u.end(), ' '), m_u.end()); }
+inline void trim() { m_u = m_u.erase(std::remove(m_u.begin(), m_u.end(), ' '), m_u.end()); }
 
-    friend String operator+(const String&, const String&);
-    [[nodiscard]] inline bool equals(const String& s) const noexcept { return m_u == s.m_u; }
-    [[nodiscard]] inline bool equalsIgnoreCase(const String& s) const noexcept { 
-        String str = m_u;
-        return (std::transform(s.begin(), s.end(), s.begin(), std::tolower) == std::transform(str.begin(), str.end(), str.begin(), std::tolower)); 
-    }
+[[nodiscard]] inline bool equals(const String& s) const noexcept { return m_u == s.m_u; }
 
-    [[nodiscard]] inline bool operator==(const String& s) const noexcept { return m_u == s.m_u; }
-    [[nodiscard]] inline bool operator!=(const String& s) const noexcept { return m_u != s.m_u; }
-    [[nodiscard]] inline bool operator<(const String& s) const noexcept { return m_u < s.m_u; }
-    [[nodiscard]] inline bool operator<=(const String& s) const noexcept { return m_u <= s.m_u; }
-    [[nodiscard]] inline bool operator>(const String& s) const noexcept { return m_u > s.m_u; }
-    [[nodiscard]] inline bool operator>=(const String& s) const noexcept { return m_u >= s.m_u; }
+[[nodiscard]] inline bool equalsIgnoreCase(const String& s) const noexcept {
+    String str = m_u;
+    return (std::transform(s.begin(), s.end(), s.begin(), std::tolower) == std::transform(str.begin(), str.end(), str.begin(), std::tolower));
+}
 
-    friend String operator+(const String&, const char*);
-    friend String operator+(const char*, const String&);
-};
+[[nodiscard]] inline bool operator==(const String& s) const noexcept { return m_u == s.m_u; }
+[[nodiscard]] inline bool operator!=(const String& s) const noexcept { return m_u != s.m_u; }
+[[nodiscard]] inline bool operator<(const String& s) const noexcept { return m_u < s.m_u; }
+[[nodiscard]] inline bool operator<=(const String& s) const noexcept { return m_u <= s.m_u; }
+[[nodiscard]] inline bool operator>(const String& s) const noexcept { return m_u > s.m_u; }
+[[nodiscard]] inline bool operator>=(const String& s) const noexcept { return m_u >= s.m_u; }
 
 [[nodiscard]] inline String operator+(const String& lhs, const String& rhs) { return lhs + rhs; }
 [[nodiscard]] inline String operator+(const String& lhs, const char* rhs) { return lhs + rhs; }
 [[nodiscard]] inline String operator+(const char* lhs, const String& rhs) { return lhs + rhs; }
-
-#endif // SMARTCAR_EMUL_STRING_CXX
