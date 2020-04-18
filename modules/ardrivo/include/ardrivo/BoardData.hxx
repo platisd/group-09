5e84b9c12761ebdc6e1a54ea6a379d43b4efafbc
Ruthger Dijt <ruthgerdijt@gmail.com>
2020-04-18 18:19:47 +0200

    [ardppolyfills] First round of fixing everything Print - Stream - Serial nice

---
+++ modules/ardrivo/include/ardrivo/BoardData.hxx
@@ -0,0 +1,32 @@
+/*
+ *  BoardData.hxx
+ *  Copyright 2020 ItJustWorksTM
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+#ifndef SMARTCAR_EMUL_BOARDDATA_HXX
+#define SMARTCAR_EMUL_BOARDDATA_HXX
+
+#include <vector>
+#include <deque>
+
+extern bool (*write_byte)(unsigned char);
+extern size_t (*write_buf)(const unsigned char*, size_t);
+
+struct BoardData {
+    std::vector<char> data;
+};
+
+#endif // SMARTCAR_EMUL_BOARDDATA_HXX
