0a6fff6d67475a4d19fce2c7556abd19de1c5391
Rongzhen Chen <45301045+crz11111@users.noreply.github.com>
2020-04-18 09:27:44 +0200

    BoardData

---
+++ modules/ardrivo/include/ardrivo/BoardData.h
@@ -0,0 +1,12 @@
+//
+// Created by Coulson Chen on 2020-04-18.
+//
+
+#ifndef SMARTCAR_EMUL_BOARDDATA_H
+#define SMARTCAR_EMUL_BOARDDATA_H
+
+#include <vector>
+
+struct BoardData{std::vector<char> data;};
+
+#endif // SMARTCAR_EMUL_BOARDDATA_H
