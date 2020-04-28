b0b0e4764e4aed44c6e59c5ee3f5077361cc6dd4
AeroStun <24841307+AeroStun@users.noreply.github.com>
2020-04-28 23:09:27 +0200

    [ardrivo] SPI impl

--- include/BoardData.hxx
+++ include/BoardData.hxx
@@ -22,8 +22,12 @@
 #include <array>
 #include <atomic>
 #include <chrono>
+#include <condition_variable>
+#include <functional>
 #include <mutex>
+#include <unordered_map>
 #include <utility>
+#include <variant>
 #include <vector>
 
 struct BidirMutexes {
@@ -44,8 +48,18 @@ struct DynaBufferBus : BidirMutexes {
     std::vector<std::byte> tx;
 };
 
+template <class BufferBus>
+struct BlockingBus : BufferBus {
+    std::condition_variable sync; // C++20: squash into below atomic by means of `std::atomic::wait`
+    std::size_t request_bytes{}; // C++20: use `std::atomic_size_t` for thread-sync
+    std::mutex request_bytes_mutex; // C++20: remove
+};
+
 struct UartBus : DynaBufferBus {};
 struct I2cBus : FixBufferBus<32u> {};
+struct SpiBus {
+    std::unordered_map<std::uint16_t, std::variant<BlockingBus<DynaBufferBus>, std::function<void(std::byte*, std::size_t)>>> slaves; // GCC10: use `std::span` in funcsig
+};
 
 struct BoardData {
     std::vector<std::atomic_bool> digital_pin_values;
@@ -54,6 +68,7 @@ struct BoardData {
     std::vector<std::atomic_uint8_t> pin_frequency;
     std::vector<UartBus> uart_buses;
     std::vector<I2cBus> i2c_buses;
+    std::vector<SpiBus> spi_buses;
     std::vector<std::atomic_uint8_t> pin_modes;
 
     bool (*write_byte)(unsigned char){};
